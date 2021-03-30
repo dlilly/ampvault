f00d71fe5d6f7a5ecbc4d04282ff05dbb8349ccd
dave lilly <dave.lilly@gmail.com>
2021-03-30 12:47:20 -0500

    updates to backend classes

--- backends/bigcommerce.js
+++ backends/bigcommerce.js
@@ -4,8 +4,50 @@ const _ = require('lodash')
 module.exports = cred => {
     let catalogApiUrl = `${cred.apiUrl}/stores/${cred.storeHash}/v3/catalog`
 
+    // mappers
+    let mapImage = image => ({ url: image.url_standard })
+
+    let mapCategory = cat => ({
+        name: cat.name,
+        id: cat.id
+    })
+
+    let mapProduct = prod => ({
+        id: prod.id,
+        name: prod.name,
+        shortDescription: prod.description,
+        longDescription: prod.description,
+        variants: _.map(prod.variants, mapVariant(prod))
+    })
+
+    let mapVariant = prod => variant => {
+        let images = variant.image_url ? [{ url: variant.image_url }] : _.map(prod.images, mapImage)
+        return {
+            id: variant.id,
+            sku: variant.sku,
+            prices: {
+                list: variant.price || prod.price,
+                sale: variant.sale_price || prod.price
+            },
+            defaultImage: _.first(images),
+            images
+        }
+    }
+
+    let populateCategory = async cat => ({
+        ...cat,
+        products: _.get(await getProducts({ "categories:in": cat.id }), 'results'),
+        children: _.get(await bc.categories.get({ opts: { parent_id: cat.id }, mapper: mapCategory }), 'results')
+    })
+    // end mappers
+
     // makes a request to the BC API with the given uri and formats the response data with the given mapper
-    let makeCatalogAPIRequest = async ({ uri, opts, mapper }) => {
+    let makeCatalogAPIRequest = async ({ uri, opts = {}, mapper }) => {
+        if (opts.limit && opts.offset) {
+            opts.page = Math.floor((opts.offset / opts.limit) + 1)
+            delete opts.offset
+        }
+
         let queryString = '?'
         _.each(opts, (v, k) => {
             let vs = Array.isArray(v) ? v : [v]
@@ -19,34 +61,32 @@ module.exports = cred => {
             })
         })
         
-        const response = await axios(`${catalogApiUrl}${uri}${queryString}`, { headers: { 'X-Auth-Token': cred.apiToken } });
+        let url = `${catalogApiUrl}${uri}${queryString}`
+        const response = await axios(url, { headers: { 'X-Auth-Token': cred.apiToken } });
+
         if (!Array.isArray(response.data.data)) {
             return await mapper(response.data.data)
         }
         else {
-            if (opts.sku) {
-                return await mapper(_.first(response.data.data))
-            }
-            else {
-                let results = await Promise.all(response.data.data.map(await mapper))
-                return {
-                    total: response.data.meta.pagination.total,
-                    count: response.data.meta.pagination.count,
-                    limit: response.data.meta.pagination.per_page,
-                    offset: (response.data.meta.pagination.current_page - 1) * response.data.meta.pagination.per_page,
-                    results
-                }
+            let results = await Promise.all(response.data.data.map(await mapper))
+            return {
+                total: response.data.meta.pagination.total,
+                count: response.data.meta.pagination.count,
+                limit: response.data.meta.pagination.per_page,
+                offset: (response.data.meta.pagination.current_page - 1) * response.data.meta.pagination.per_page,
+                results
             }
         }
     }
 
-    return {
+    let bc = {
         products: {
-            get: async opts => await makeCatalogAPIRequest({ ...opts, uri: '/products' })
+            get: async opts => await makeCatalogAPIRequest({ ...opts, uri: '/products', mapper: opts.mapper || mapProduct })
         },
         categories: {
-            get: async opts => await makeCatalogAPIRequest({ ...opts, uri: '/categories' })
+            get: async opts => await makeCatalogAPIRequest({ ...opts, uri: '/categories', mapper: opts.mapper || populateCategory })
         },
         type: 'bigcommerce'
     }
+    return bc
 }
--- backends/commercetools.js
+++ backends/commercetools.js
@@ -22,13 +22,86 @@ module.exports = cred => {
     const httpMiddleware = createHttpMiddleware({ host: cred.api_url, fetch })
     const client = createClient({ middlewares: [authMiddleware, httpMiddleware] })
     
+    let getLocalizedText = text => text['en'] || _.first(text)
+
+    let mapImage = image => image && ({ url: image.url })
+    let mapVariant = variant => ({
+        id          : variant.id,
+        sku         : variant.sku,
+        prices      : { list: _.get(_.first(variant.prices), 'value.centAmount') / 100 },
+        images      : _.map(variant.images, mapImage),
+        defaultImage: mapImage(_.first(variant.images))
+    })
+
+    let mapProduct = product => ({
+        id          : `${product.id}`,
+        name        : `${getLocalizedText(product.name)}`,
+        slug        : `${getLocalizedText(product.slug)}`,
+        variants    : _.map(_.concat(product.variants, [product.masterVariant]), mapVariant),
+        categories  : _.map(product.categories, mapCategory)
+    })
+
+    let mapCategory = category => {
+        let cat = category.obj || category
+        return {
+            id      : category.id,
+            name    : getLocalizedText(cat.name),
+            slug    : getLocalizedText(cat.slug) 
+        }
+    }
+
+    let populateCategory = async (category) => {
+        let cat = mapCategory(category)
+
+        // get the child products
+        let products = (await rb.productProjections.get({ where: [`categories(id="${category.id}")`], expand: ['categories[*]'] })).results
+    
+        // get the child categories
+        let children = (await rb.categories.get({ where: [`parent(id="${category.id}")`] }, {}, mapCategory)).results
+    
+        return {
+            ...cat,
+            products,
+            children
+        }
+    }
+
+    let mappers = {
+        productProjections: mapProduct,
+        productProjectionsSearch: mapProduct,
+        categories: populateCategory
+    }
+
     let rb = createRequestBuilder({ projectKey: cred.project })
-    _.each(Object.values(rb), operation => {
-        operation.get = async (opts, query) => {
+    _.each(Object.keys(rb), key => {
+        let operation = rb[key]
+        operation.get = async (opts, query, mapper = mappers[key] || (x => x)) => {
+            console.log(`opts ${JSON.stringify(opts)}`)
+            console.log(`query ${JSON.stringify(query)}`)
+            console.log(`op ${Object.keys(operation)}`)
+
             let uri = operation.parse(opts).build()
             let separator = uri.indexOf("?") > -1 ? "&" : "?"
             uri  = `${uri}${separator}${_.map(query, (v, k) => `${k}=${v}`).join("&")}`
-            return await client.execute({ uri, method: 'GET' })
+
+            console.log(`ct ${uri}`)
+
+            let { body } = await client.execute({ uri, method: 'GET' })
+
+            if (query.sku || query.id || query.slug) {
+                let result = _.first(body.results)
+                console.log(`result ${JSON.stringify(result)}`)
+                return await mapper(result)
+            }
+            else {
+                return {
+                    limit: body.limit,
+                    count: body.count,
+                    offset: body.offset,
+                    total: body.total,
+                    results: await Promise.all(body.results.map(await mapper))
+                }
+            }
         }
     })
     return {
